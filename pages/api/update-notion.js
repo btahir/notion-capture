9a13f0b85ffd81f77a8e6c230a96cd7671413329
btahir89 <bilal.tahir89@gmail.com>
2021-05-14 16:42:12 -0700

    dont need axios

--- pages/api/update-notion.js
+++ pages/api/update-notion.js
@@ -1,15 +1,13 @@
-import axios from 'axios'
-
 export default async function (req, res) {
   try {
-    await axios('https://api.notion.com/v1/pages', {
+    await fetch('https://api.notion.com/v1/pages', {
       method: 'POST',
       headers: {
         'Content-type': 'application/json',
         Authorization: `Bearer ${process.env.MY_NOTION_TOKEN}`,
         'Notion-Version': '2021-05-13',
       },
-      data: JSON.stringify({
+      body: JSON.stringify({
         parent: { database_id: `${process.env.DATABASE_ID}` },
         properties: {
           Email: {
